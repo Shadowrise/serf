f0f545df8dc1fff4c6dc3bb79dbcd2aa317c4ac1
Viacheslav Emtcov <viacheslav.emtkov@datium.fi>
2019-11-06 23:13:38 +0300

    p2 1:25

--- app/js/main.js
+++ app/js/main.js
@@ -14,4 +14,11 @@ $(function(){
         asNavFor: ".header__slider"
     });
 
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">'
+    });
+
 });
